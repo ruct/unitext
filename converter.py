87f6b0078d8737b8d9d59a4e13d0c81806408299
ltry <flyce32@yandex.ru>
2019-10-23 22:32:58 +0300

    some convertions implemented

--- converter.py
+++ converter.py
@@ -1,10 +1,21 @@
+import io
+import magic
 import pypandoc
 import mimetypes
-import magic
-from flask import request, Flask
+from flask import send_file, request, abort, Flask
+
+new_ext = {
+    "md": "md",
+    "dot": "docx",
+    "wiz": "docx",
+    "doc": "docx",
+    "docx": "docx",
+}
 
 app = Flask(__name__)
-@app.route("/to/<out_type>", methods = ["POST", "GET"])
+
+
+@app.route("/to/<out_type>", methods=["POST", "GET"])
 def convert(out_type):
     print(out_type)
     if request.method == "GET":
@@ -25,7 +36,27 @@ def convert(out_type):
     data = file.read()
     mime = magic.from_buffer(data, True)
     ext = mimetypes.guess_extension(mime)[1:]
-    return ext
+    print(ext)
+    if ext not in new_ext:
+        abort(400, "File format " + ext + " isn't supported")
+    ext = new_ext[ext]
+    print("New ext = " + ext)
+
+    res_name = file.filename
+    if res_name.count("."):
+        res_name = res_name[:res_name.rfind(".")]
+
+    res_name += "." + ext
+    print(res_name)
+
+    cdata = str.encode(pypandoc.convert(data,
+                                        out_type,
+                                        format=ext))
+
+    return send_file(io.BytesIO(cdata),
+                     mimetype=mimetypes.guess_type(res_name)[0],
+                     as_attachment=True,
+                     attachment_filename=res_name)
 
 
 if __name__ == "__main__":
